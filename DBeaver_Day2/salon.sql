3dd1d3e77c7e7b6a9165f5d119b29f054b6957d3
Franko <franko.vekic@gmail.com>
2021-05-12 18:32:22 +0200

    -

--- DBeaver_Day2/salon.sql
+++ DBeaver_Day2/salon.sql
@@ -9,21 +9,22 @@ adresa varchar(50),
 djelatnica int
 );
 
-create table djelatnica(
+create table osoba(
 sifra int not null primary key auto_increment,
 ime varchar(50) not null,
-prezime varchar(50) not null,
+prezime varchar(50) not null
+);
+
+create table djelatnica(
+sifra int not null primary key auto_increment,
+osoba int not null,
 oib varchar(11),
-iban varchar(32),
-korisnik int
+iban varchar(32)
 );
 
 create table korisnik(
 sifra int not null primary key auto_increment,
-ime varchar(50) not null,
-prezime varchar(50) not null,
-oib varchar(11),
-usluga int
+osoba int
 );
 
 create table usluga(
@@ -33,6 +34,15 @@ cijena decimal (18,2),
 trajanje datetime
 );
 
+create table posjet(
+korisnik int,
+djelatnica int,
+usluga int
+);
+
 alter table salon add foreign key (djelatnica) references djelatnica(sifra);
-alter table djelatnica add foreign key (korisnik) references korisnik(sifra);
-alter table korisnik add foreign key (usluga) references usluga(sifra);
+alter table djelatnica add foreign key (osoba) references osoba(sifra);
+alter table korisnik add foreign key (osoba) references osoba(sifra);
+alter table posjet add foreign key (korisnik) references korisnik(sifra);
+alter table posjet add foreign key (djelatnica) references djelatnica(sifra);
+alter table posjet add foreign key (usluga) references usluga(sifra);
