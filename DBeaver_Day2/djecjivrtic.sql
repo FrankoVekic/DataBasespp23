35f7a84eb91578d24b823dacb527bccd0a47eab1
Franko <franko.vekic@gmail.com>
2021-05-12 18:20:22 +0200

    -

--- DBeaver_Day2/djecjivrtic.sql
+++ DBeaver_Day2/djecjivrtic.sql
@@ -13,17 +13,15 @@ create table djete(
 sifra int not null primary key auto_increment,
 ime varchar(50) not null,
 prezime varchar(50) not null,
-godina int,
-oib varchar(11),
-skupina int,
-odgojitelj int
+datum_rodenja date not null,
+oib varchar(11)
 );
 
 create table skupina(
 sifra int not null primary key auto_increment,
 naziv varchar(50) not null,
-djete int,
-odgojitelj int
+djete int not null,
+odgojitelj int not null
 );
 
 create table odgojitelj(
@@ -32,8 +30,7 @@ ime varchar(50) not null,
 prezime varchar(50) not null,
 oib varchar(11),
 iban varchar(32),
-strucna_sprema int,
-skupina int
+strucna_sprema int
 );
 
 create table strucna_sprema(
@@ -43,9 +40,5 @@ strucna_sprema varchar(20)
 
 alter table vrtic add foreign key (skupina) references skupina(sifra);
 alter table skupina add foreign key (djete) references djete(sifra);
-alter table djete add foreign key (odgojitelj) references odgojitelj(sifra);
+alter table skupina add foreign key (odgojitelj) references odgojitelj(sifra);
 alter table odgojitelj add foreign key (strucna_sprema) references strucna_sprema(sifra);
-
-
-
-
