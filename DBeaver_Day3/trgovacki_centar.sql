58e7d33f22d6ef9979238187c6c0c463cc679161
Franko <franko.vekic@gmail.com>
2021-05-12 17:50:20 +0200

    update

--- DBeaver_Day3/trgovacki_centar.sql
+++ DBeaver_Day3/trgovacki_centar.sql
@@ -20,23 +20,29 @@ oib varchar(11) not null
 create table trgovina(
 sifra int not null primary key auto_increment,
 naziv varchar(50),
-prostor int,
-sef int
+prostor int not NULL,
+sef int not null
 );
 
 create table sef(
 sifra int not null primary key auto_increment,
-osoba int,
+osoba int not null,
 iban varchar(32)
 );
 
 create table radnik(
-osoba int,
-trgovina int
+sifra int not null primary key auto_increment,
+osoba int not null,
+trgovina int not null
+);
+create table rad(
+radnik int not null,
+trgovina int not null
 );
 
 alter table trgovacki_centar add foreign key (trgovina) references trgovina(sifra);
 alter table radnik add foreign key (osoba) references osoba(sifra);
-alter table radnik add foreign key (trgovina) references trgovina(sifra);
 alter table trgovina add foreign key (sef) references sef(sifra);
 alter table sef add foreign key (osoba) references osoba(sifra);
+alter table rad add foreign key (radnik) references radnik(sifra);
+alter table rad add foreign key (trgovina) references trgovina(sifra);
