6d0f58be2e7dc29cf3818b6f6906b09c067ab88d
Franko Vekić <franko.vekic@gmail.com>
2021-05-14 20:57:30 +0200

    Correcting mistakes

--- DBeaver_Day5/odvjetnik.sql
+++ DBeaver_Day5/odvjetnik.sql
@@ -10,12 +10,12 @@ iban varchar(32) not null
 
 create table klijent (
 sifra int not null primary key auto_increment,
-osoba int
+osoba int not null
 );
 
 create table suradnik (
 sifra int not null primary key auto_increment,
-osoba int
+osoba int not null
 );
 
 create table osoba(
@@ -28,11 +28,16 @@ oib char(11)
 );
 
 create table obrana(
-odvjetnik int,
-suradnik int,
-klijent int,
+sifra int not null primary key auto_increment,
+odvjetnik int not null,
+klijent int not null,
 datumvrijeme datetime,
 cijena decimal(18,2)
+);
+
+create table suradnik_obrana(
+suradnik int not null,
+obrana int not null
 );
 
 alter table odvjetnik add foreign key (osoba) references osoba(sifra);
@@ -40,7 +45,9 @@ alter table klijent add foreign key (osoba) references osoba(sifra);
 alter table suradnik add foreign key (osoba) references osoba(sifra);
 alter table obrana add foreign key (odvjetnik) references odvjetnik(sifra);
 alter table obrana add foreign key (klijent) references klijent(sifra);
-alter table obrana add foreign key (suradnik) references suradnik(sifra);
+alter table suradnik_obrana add foreign key (suradnik) references suradnik(sifra);
+alter table suradnik_obrana add foreign key (obrana) references obrana(sifra);
+
 
 insert into osoba (ime,prezime,datumrodenja)
 values 
@@ -51,11 +58,17 @@ values
 ('Marta','Martić','1992-05-10'),
 ('Mirko','Mirić','1992-05-10');
 
+
 insert into odvjetnik (osoba,iban) values (1,'HR94 4115 0081 0111 1111 6'),(6,'HR94 4115 0081 01231 1000 2');
 insert into klijent (osoba) values (4),(5);
 insert into suradnik (osoba) values (2),(3);
-insert into obrana (odvjetnik,suradnik,klijent,cijena) 
+insert into obrana (odvjetnik,klijent,cijena) 
 values
-(2,2,1,9999.99),(1,1,2,2000.00);
+(2,2,9999.99),(1,1,2000.00);
+insert into suradnik_obrana (suradnik,obrana) values (1,2),(2,1),(1,1);
+
+update osoba set ime='Petar' where sifra=1;
+update osoba set prezime='Miloš' where sifra=6;
+delete from suradnik_obrana where suradnik=2;
 
 
